3265450edd72239aa0215252946b9aec0b3b38b2
Vyacheslav Solomin <slel64222@gmail.com>
2018-11-06 03:26:01 +0300

    Реализовал action - событие view в архтитектуре flux

---
+++ drowsy/src/action.ts
@@ -0,0 +1,29 @@
+export type ActionType = string;
+export type ActionData = string | number | object | boolean | null;
+
+// На лекции говорилось что надо через абстрактный класс реализовывать
+// ммм непонятно почему, а точнее как и зачем
+
+export class Action {
+    private type: ActionType;
+    private data: ActionData;
+
+    constructor(type: ActionType = "defultAction", data: ActionData = null) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public getType() {
+        return this.type;
+    }
+
+    public getData() {
+        return this.data;
+    }
+
+    public setData(data: ActionData) {
+        this.data = data;
+    }
+}
+
+export default Action;
